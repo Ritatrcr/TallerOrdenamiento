62f52d59b4483f2d9abfa745701a9ffafe4c9451
Rita Trindade da Cruz <ritatrcr@unisabana.edu.co>
2024-03-18 09:36:51 -0500

    Explicación punto A

--- Punto_A.py
+++ Punto_A.py
@@ -19,12 +19,11 @@ print(sorted_personajes)
 
 """
 
-Para ordenar alfabéticamente una lista de nombres, un buen método de ordenamiento es el algoritmo de ordenamiento Quick Sort. Aquí están las razones por las que Quick Sort podría ser considerado el mejor para este propósito:
-
-Eficiencia en promedio: Quick Sort tiene un rendimiento muy bueno en promedio y es eficiente para listas grandes. En el caso promedio, tiene una complejidad de tiempo de O(n log n), lo que lo hace adecuado para ordenar listas de nombres.
-
-Eficiente para datos casi ordenados: Si los nombres ya están casi en orden alfabético, Quick Sort tiende a funcionar muy bien, lo que puede ser común en listas de nombres.
-
-Menor uso de memoria: Quick Sort es un algoritmo in-place, lo que significa que no requiere una cantidad significativa de memoria adicional para ordenar la lista. Esto puede ser ventajoso cuando se trabaja con grandes conjuntos de datos.
-
-Implementación simple y compacta: La implementación de Quick Sort puede ser relativamente simple y fácil de entender, lo que facilita su mantenimiento y comprensión."""
+Se implementa Quick Sort ya que es eficiente para listas grandes y utiliza menos memoria.
+El algoritmo de Quick Sort es un algoritmo de ordenamiento que utiliza la técnica de divide y vencerás para ordenar listas.
+El algoritmo funciona de la siguiente manera:
+1. Se elige un elemento de la lista al que llamaremos pivote.
+2. Se reorganiza la lista de manera que todos los elementos con valores menores al pivote queden a su izquierda y los mayores a su derecha.
+3. Los elementos de la lista son ordenados de manera independiente en cada lado del pivote.
+4. Finalmente, la lista queda ordenada.
+"""
