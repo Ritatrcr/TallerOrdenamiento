000014bd3ce9111b42795a23ba5fc87d50a3cbea
Rita Trindade da Cruz <ritatrcr@unisabana.edu.co>
2024-03-19 13:23:46 -0500

    realizacion de funciones

--- Simpson.py
+++ Simpson.py
@@ -1,5 +1,6 @@
 #Lista Simpson
-
+lista=["bola de nive", "Bart","Lisa","Homero","Marge","Krusty","Rodd","Todd","Ned Flaters", "Moe","Barnie", 
+       " Skinew", "Milkhouse", "Nelsay", "Rafa"]
 
 #Crear Menu 
 def menu(): 
@@ -11,10 +12,10 @@ def menu():
     print("6. Salir")
     return int(input("Digite la opcion que desea realizar: "))
 
+
 def quick_sort(arr):
     if len(arr) <= 1:
         return arr
-    
     pivot = arr[len(arr) // 2]
     left = [x for x in arr if x < pivot]
     middle = [x for x in arr if x == pivot]
@@ -22,25 +23,40 @@ def quick_sort(arr):
     
     return quick_sort(left) + middle + quick_sort(right)
 
-def quick_sort_desc(arr):
-    if len(arr) <= 1:
-        return arr
-    
-    pivot = arr[len(arr) // 2]
-    left = [x for x in arr if x > pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x < pivot]
-    
-    return quick_sort_desc(left) + middle + quick_sort_desc(right)
+def ordena_forma_ascendente(arr):
+    return quick_sort(arr)
 
-def quick_sort_letra(arr, letra):
-    if len(arr) <= 1:
-        return arr
-    
-    pivot = arr[len(arr) // 2]
-    left = [x for x in arr if x[0] < letra]
-    middle = [x for x in arr if x[0] == letra]
-    right = [x for x in arr if x[0] > letra]
-    
-    return quick_sort_letra(left, letra) + middle + quick_sort_letra(right, letra)
+def ordena_forma_descendente(arr):
+    return quick_sort(arr)[::-1]
 
+#def ordenar_desde_personaje_con_letra(arr, letra):
+
+def personaje_pedido_por_consola(arr, nombre):
+    if nombre in arr:
+        return nombre
+    else:
+        return "No se encuentra el personaje"
+
+def personaje_por_medio_de_indice(arr, indice):
+    if indice < len(arr):
+        return arr[indice]
+    else:
+        return "No se encuentra el personaje"
+
+def innit():
+    opcion = menu()
+    while opcion != 6:
+        if opcion == 1:
+            print(ordena_forma_ascendente(lista))
+        elif opcion == 2:
+            print(ordena_forma_descendente(lista))
+        elif opcion == 3:
+            letra = input("Digite la letra por la que desea ordenar: ")
+            #que hace??
+        elif opcion == 4:
+            nombre = input("Digite el nombre del personaje que desea buscar: ")
+            print(personaje_pedido_por_consola(lista, nombre))
+        elif opcion == 5:
+            indice = int(input("Digite el indice del personaje que desea buscar: "))
+            print(personaje_por_medio_de_indice(lista, indice))
+        opcion = menu()
